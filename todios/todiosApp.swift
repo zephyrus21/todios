39a4468ec60c9abc42813a404347cdcaae2c0eaa
zephyrus21 <pandey.piyus@gmail.com>
2022-11-17 10:48:36 +0530

    Initial Commit

---
+++ todios/todiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  todiosApp.swift
+//  todios
+//
+//  Created by Piyush Pandey on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct todiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
